6b57fb10063d1332e3ddf1ac8613afe64495964e
noahturner02 <noahturner02@gmail.com>
2023-10-10 01:02:08 -0500

    Added a title and added form validations

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,34 +1,57 @@
 <template>
   <v-container class="d-flex justify-center" style="row-gap: 50px">
     <v-card class="align-content-center pa-5" style="width: 40%">
+      <v-card-title class="text-xl-center mb-3">Create Account</v-card-title>
       <v-form>
         <div
           class="d-flex justify-center"
           style="flex-direction: column; align-items: center"
         >
-          <v-text-field class="formfield" label="First Name"></v-text-field>
-          <v-text-field class="formfield" label="Last Name"></v-text-field>
+          <v-text-field
+            v-model="firstName"
+            class="formfield"
+            label="First Name"
+            :error-messages="v$.firstName.$errors.map((e) => e.$message)"
+          ></v-text-field>
+          <v-text-field
+            v-model="lastName"
+            class="formfield"
+            label="Last Name"
+            :error-messages="v$.lastName.$errors.map((e) => e.$message)"
+          ></v-text-field>
           <v-text-field
             v-model="phoneNumber"
             class="formfield"
             label="Phone Number"
             type="number"
+            :error-messages="v$.phoneNumber.$errors.map((e) => e.$message)"
           ></v-text-field>
           <v-text-field
+            v-model="password"
             class="formfield"
             label="Password"
             type="password"
+            :error-messages="v$.password.$errors.map((e) => e.$message)"
           ></v-text-field>
           <v-text-field
+            v-model="verifyPassword"
             class="formfield"
             label="Re-Enter Password"
             type="password"
+            :error-messages="v$.verifyPassword.$errors.map((e) => e.$message)"
           ></v-text-field>
-          <v-text-field class="formfield" label="Address"></v-text-field>
           <v-text-field
+            v-model="address"
+            class="formfield"
+            label="Address"
+            :error-messages="v$.address.$errors.map((e) => e.$message)"
+          ></v-text-field>
+          <v-text-field
+            v-model="email"
             class="formfield"
             label="Email"
             type="email"
+            :error-messages="v$.email.$errors.map((e) => e.$message)"
           ></v-text-field>
           <div class="d-flex justify-center" style="gap: 40px">
             <v-btn
@@ -40,7 +63,7 @@
             <v-btn
               size="x-large"
               style="background-color: darkgreen; width: 250px"
-              @click="$emit('create-account')"
+              @click="submitForm"
               >Create Account</v-btn
             >
           </div>
@@ -52,6 +75,8 @@
 
 <script>
 import { VForm, VTextField, VCard } from "vuetify/lib/components/index.mjs";
+import { useVuelidate } from "@vuelidate/core";
+import { required, email, minLength, sameAs } from "@vuelidate/validators";
 export default {
   name: "SignupForm",
   components: {
@@ -60,9 +85,18 @@ export default {
     VCard
   },
   emits: ["exit"],
+  setup() {
+    return { v$: useVuelidate() };
+  },
   data() {
     return {
-      phoneNumber: undefined
+      firstName: undefined,
+      lastName: undefined,
+      phoneNumber: undefined,
+      password: undefined,
+      verifyPassword: undefined,
+      address: undefined,
+      email: undefined
     };
   },
   watch: {
@@ -71,6 +105,31 @@ export default {
         this.$nextTick(() => {
           this.phoneNumber = this.phoneNumber.toString().slice(0, 10);
         });
+      }
+    }
+  },
+  validations() {
+    return {
+      firstName: { required },
+      lastName: { required },
+      phoneNumber: { required, minLength: minLength(10) },
+      password: {
+        required,
+        minLength: minLength(10)
+      },
+      verifyPassword: {
+        required,
+        sameAs: sameAs(this.password)
+      },
+      address: { required },
+      email: { required, email }
+    };
+  },
+  methods: {
+    async submitForm() {
+      const isFormCorrect = await this.v$.$validate();
+      if (!isFormCorrect) {
+        console.log("Failed to submit. Validations failed!");
       }
     }
   }
