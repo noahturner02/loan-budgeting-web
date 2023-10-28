a5861b9669796814c793c5a5d2e02990310cc5c8
noahturner02 <noahturner02@gmail.com>
2023-10-28 13:46:17 -0500

    Install Material Design Iconset (finally)

--- src/main.js
+++ src/main.js
@@ -6,14 +6,21 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
-import 'material-design-icons-iconfont/dist/material-design-icons.css';
-
+import { aliases, mdi } from "vuetify/iconsets/mdi"
+import '@mdi/font/css/materialdesignicons.css'
 const vuetify = createVuetify({
   components,
   directives,
   theme: {
     defaultTheme: 'dark',
   },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    }
+  }
 });
 
 const pinia = createPinia();
