c09b532e6827ccfa855c8c4f2881dcab72b969b9
noahturner02 <noahturner02@gmail.com>
2023-11-06 17:29:32 -0600

    Beef up pwd requirements.
    Add alert for inputting existing username or password

--- src/components/ChangePasswordDialog.vue
+++ src/components/ChangePasswordDialog.vue
@@ -2,8 +2,8 @@
     <v-card class="pa-5">
         <v-card-title class="text-xl-center">Change Password</v-card-title>
         <v-card-text class="d-flex" style="flex-direction: column;">
-            <v-text-field v-model="verifyPassword" type="password" label="Verify Password" :error-messages="v$.verifyPassword.$errors.map((e) => e.$message)"></v-text-field>
-            <v-text-field v-model="newPassword" type="password" label="New Password" :error-messages="v$.newPassword.$errors.map((e) => e.$message)"></v-text-field>
+            <v-text-field v-model="verifyPassword" type="password" label="Verify Password" :error-messages="v$.verifyPassword.$errors.map((e) => e.$message)" @blur="v$.verifyPassword.$touch"></v-text-field>
+            <v-text-field v-model="newPassword" type="password" label="New Password" :error-messages="v$.newPassword.$errors.map((e) => e.$message)" @blur="v$.newPassword.$touch"></v-text-field>
             <div class="d-flex justify-center" style="gap: 100px">
                 <v-btn style="background-color: firebrick" size="x-large" @click="$emit('closeForm')">Cancel</v-btn>
                 <v-btn style="background-color: darkgreen" size="x-large" @click="submitAction()">Submit</v-btn>
@@ -15,7 +15,7 @@
 import { editCustomer } from '@/api/api';
 import { useCustomerStore } from '@/stores/customerStore';
 import useVuelidate from '@vuelidate/core';
-import { required, minLength, sameAs } from '@vuelidate/validators';
+import { required, minLength, sameAs, helpers } from '@vuelidate/validators';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -40,7 +40,14 @@ export default defineComponent({
             },
             newPassword: {
                 required,
-                minLength: minLength(10),
+                minLength: minLength(8),
+                valid: helpers.withMessage('Password must have at least one uppercase, one lowercase, one number, and one special character', function(value) {
+                    const containsuppercase = /[a-z]/.test(value)
+                    const containslowercase = /[a-z]/.test(value)
+                    const containsnumber = /[0-9]/.test(value)
+                    const containsspecial = /[#?!@$%^&*-]/.test(value)
+                    return containsuppercase && containslowercase && containsnumber && containsspecial
+                })
             }
         }
     },
